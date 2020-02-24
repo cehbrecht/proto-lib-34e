08b3346f2539a93b016ef19eb0d372aaa3f17391
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-24 19:15:21 +0100

    update tests

--- tests/test_daops_subset.py
+++ tests/test_daops_subset.py
@@ -4,4 +4,12 @@ CMIP5_ID1 = 'cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.ta
 
 
 def test_subset_t():
-    result = daops.subset(CMIP5_ID1, time=('2085-01-01', '2120-12-30'), output_dir='outputs')
+    result = daops.subset(CMIP5_ID1,
+                          time=('2085-01-01', '2120-12-30'),
+                          output_dir='outputs')
+
+def test_subset_t_y_x():
+    result = daops.subset(CMIP5_ID1,
+                          time=('2085-01-01', '2120-12-30'),
+                          space=('-20', '-10', '5', '15'),
+                          output_dir='outputs')
