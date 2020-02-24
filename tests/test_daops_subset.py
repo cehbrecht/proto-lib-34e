b01525f1b98d02ab7edce496011e32baedea23fd
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-24 14:38:31 +0100

    added daops test

---
+++ tests/test_daops_subset.py
@@ -0,0 +1,7 @@
+import daops
+
+CMIP5_ID1 = 'cmip5.output1.MOHC.HadGEM2-ES.rcp85.mon.atmos.Amon.r1i1p1.latest.tas'
+
+
+def test_subset_t():
+    result = daops.subset(CMIP5_ID1, time=('2085-01-01', '2120-12-30'), output_dir='outputs')
